20dde8a27364b40d2eb246e6d78bd3d81d7e512e
evan.christians1 <evan.christians@global.ntt>
2020-03-09 13:57:34 +0200

    refinement: updated palette & added global event listeners

--- app/dist/scripts/main.js
+++ app/dist/scripts/main.js
@@ -14,6 +14,56 @@ window.addEventListener("load", () => {
   })
 })
 
+let last_known_scroll_position = 0;
+
+function getScrollDirection(scrollEvent) {
+  changeInHeight = scrollEvent.deltaY
+
+  return changeInHeight > 0 ? 'down': 'up'
+}
+
+let pos = 0
+
+window.addEventListener('wheel', event => {
+  window.requestAnimationFrame(() => {
+    scrollDirection = getScrollDirection(event)
+
+    if (scrollDirection == 'up') {
+      last_known_scroll_position -= 20
+      function frame() {
+        if (pos <= last_known_scroll_position) {
+          clearInterval(id);
+        } else {
+          pos -= .25;
+          document.getElementById('bar').style.width = pos + '%';
+        }
+      }
+    } else {
+      last_known_scroll_position += 20
+      function frame() {
+        if (pos >= last_known_scroll_position) {
+          clearInterval(id);
+        } else {
+          pos += .25;
+          document.getElementById('bar').style.width = pos + '%';
+        }
+      }
+    }
+
+    if (last_known_scroll_position < 0) {
+      last_known_scroll_position = 0
+    }
+
+    if (last_known_scroll_position >= 100) {
+      last_known_scroll_position = 100
+    }
+
+    var id = setInterval(frame, 2)
+  });
+
+  console.log(last_known_scroll_position)
+});
+
 gridItems.forEach(e => {
   e.addEventListener("click", ()=> {
     currentlyActive ? currentlyActive.classList.remove('focused-state') : null
