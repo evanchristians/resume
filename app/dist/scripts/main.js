62663cf47ee789a7fa36cf221003d916572c4bd7
evanchristians <evanryk@gmail.com>
2020-03-22 10:29:17 +0200

    rebuilt from template

--- app/dist/scripts/main.js
+++ app/dist/scripts/main.js
@@ -1,48 +1,4 @@
-const tooltip = document.getElementById("tooltip")
-const grid = document.getElementById("grid")
-const gridItems = document.querySelectorAll(".grid > .card")
-const gridItemsContent = document.querySelectorAll(".card > .content")
 const scrollElements = document.querySelectorAll("[scrolly]")
-console.log(scrollElements)
-let currentlyActive
-let timeOut
-
-window.addEventListener("load", () => {
-  gridItems.forEach((element, key) => {
-    setTimeout(() => {
-      element.classList.add('active-state')
-    }, 75 * key)
-  })
-})
-
-gridItems.forEach(e => {
-  e.addEventListener("click", ()=> {
-    currentlyActive ? currentlyActive.classList.remove('focused-state') : null
-    currentlyActive = e
-    currentlyActive.classList.add('focused-state')
-  })
-})
-
-gridItemsContent.forEach(e => {
-  e.addEventListener("mouseenter", () => {
-    e.classList.add('active-state')
-
-    e.childNodes[0].childNodes.forEach((c, index) => {
-      setTimeout(() => {
-        c.classList.add('active-state')
-      }, 25 + 100 * index)
-    })
-  })
-  e.addEventListener("mouseleave", () => {
-    setTimeout(() => {
-      e.classList.remove('active-state')
-
-      e.childNodes[0].childNodes.forEach((c, index) => {
-        c.classList.remove('active-state')
-      })
-    }, 250)
-  })
-})
 
 function enterView(element) {
   element.classList.add('is-in-view')
@@ -52,17 +8,21 @@ function exitView(element) {
   element.classList.remove('is-in-view')
 }
 
-var observer = new IntersectionObserver(function(entry) {
-  let element = entry[0]
-	if(element.isIntersecting === true) {
-    enterView(element.target)
-  } else {
-    exitView(element.target)
-  }
-
-}, { threshold: [.5] });
-
 scrollElements.forEach(element => {
-  console.log(element);
+  let observerTimeOut
+
+  let observer = new IntersectionObserver(function(entry) {
+    let element = entry[0]
+
+    if(element.isIntersecting) {
+      clearTimeout(observerTimeOut)
+      enterView(element.target)
+    } else {
+      observerTimeOut = setTimeout(() => {
+        exitView(element.target)
+      }, 3000)
+    }
+  }, { threshold: [.2, 1] });
+
   observer.observe(element);
 })
